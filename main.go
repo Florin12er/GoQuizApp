2885c972cc9d52dd82831c61bbb6f5f0815827de
Florin12er <apetreiflorin236@gmail.com>
2024-07-13 13:41:38 +0200

    added the csv file the readme file and the main.go file

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 
     problemLoop:
 
-     for i, p := range problems{
+    for i, p := range problems{
         var answer string
         fmt.Printf("Problem %d: %s=", i+1, p.q)
 
@@ -50,19 +50,20 @@ func main() {
         }()
         select{
         case <- tObjs.C:
-        fmt.Println()
-        break problemLoop
+            fmt.Println()
+            break problemLoop
         case iAns := <-ansC:
-        if iAns == p.a{
+            if iAns == p.a{
                 correctAnswer++
             }
-        if i == len(problems)-1{
-close(ansC)
+            if i == len(problems)-1{
+                close(ansC)
             }
     }
     }
     fmt.Printf("Your result is %d out of %d\n", correctAnswer, len(problems))
     fmt.Printf("Press enter to exit")
+    <-ansC
 }
 
 func problemParser(lines [][]string) []problem {
